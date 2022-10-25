89ff5f770a469257ccf6ddd9f00a059b5faa7d06
Stefan <hoermann@cpg-gilching.de>
2022-10-25 15:42:03 +0200

    Stand_25.10.2022
    Unsere Katze kann jetzt springen und wir haben uns die Szene "Obstacle" neu angelegt.

--- CatJump/Player.gd
+++ CatJump/Player.gd
@@ -1,19 +1,21 @@
 extends KinematicBody2D
 
 const GRAVITY = 10
-const SPEED_HEIGHT = 50
+const JUMP_SPEED = -500
 
-# motion beschreibt in einem zweidimensionalen Vektor die Bewegung des Players im 2D Feld. Hat also x und y Wert.
-var motion = Vector2.ZERO
+# velocity beschreibt in einem zweidimensionalen Vektor die Bewegung des Players im 2D Feld. Hat also x und y Wert.
+var velocity = Vector2.ZERO
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Die Gravitation wirkt sich auf den y-Wert aus
-	motion.y = motion.y + GRAVITY
+	velocity.y += GRAVITY
 	
-	# die Print-Methode ist oft hilfreich bei der Fehlersuche
-	# print(motion.y)
+	# Sprung der Katze nur, wenn die Katze auf dem Boden steht:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_SPEED
+	
 	
 	# Die Methode move_and_slide ist für das Bewegen des KinematicBody2D verantwortlich
-	move_and_slide(motion)
+	move_and_slide(velocity, Vector2.UP)
